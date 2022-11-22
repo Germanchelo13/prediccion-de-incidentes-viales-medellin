6a923af6b4348ee81a7b7b4f6ceb482e3496bae2
Germanchelo13 <103703340+Germanchelo13@users.noreply.github.com>
2022-11-22 15:04:47 -0500

    informe preliminar

--- app.R
+++ app.R
@@ -1,1 +1,14 @@
+library(DT) # print table beutefull
+library(sf) # manupulación geolocalizaciones
+library(tmap)  # graficos interactivos de mapas
+library(dplyr) # manejo de funciones
+library(shiny) # aplicacion
+library(plotly) # graficos 
+library(shinydashboard)
+library(tidyverse)
+library(shinycssloaders)# to add a loader while graph is populating
 
+mapa_medellin<-st_read('Barrio_Vereda.shp')
+
+
+!T
